e801f6d38743f0992a5b544943c8c80b4950273a
Rajnish Dargan <rajnish.code30@gmail.com>
2022-05-19 18:30:41 +0530

    Issue #SB-29776 feat: editor intergration

--- src/app/components/questionset-editor/questionset-editor.component.ts
+++ src/app/components/questionset-editor/questionset-editor.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { questionSetEditorConfig } from './data';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-questionset-editor',
   templateUrl: './questionset-editor.component.html',
@@ -8,10 +8,17 @@ import { questionSetEditorConfig } from './data';
 })
 export class QuestionsetEditorComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router: Router) { }
   public editorConfig: any = questionSetEditorConfig;
 
   ngOnInit(): void {
   }
 
+  editorEventListener(event): any{
+    if (event.action === 'backContent') {
+      console.log('editor event', event);
+      this.router.navigateByUrl('/');
+    }
+  }
 }
